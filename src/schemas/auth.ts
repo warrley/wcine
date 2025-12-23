615a4779263afef71144aca786416e90bd7b0290
warrley <warleeyfarias@gmail.com>
2025-12-23 11:48:55 -0300

    node updated to v20

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const signinSchema = z.object({
     username: z.string("required"),
-    age: z.number("must be a number").min(18, "must be greater than 18"),
+    age: z.number("must be a number").min(18, "must be greater than or equal 18"),
     email: z.email("invalid email format"),
     password: z.string("required")
 });
