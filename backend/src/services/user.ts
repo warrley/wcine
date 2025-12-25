9e193f1bc5e0e932d082e9b510d2c4ca25f681e0
warrley <warleeyfarias@gmail.com>
2025-12-25 10:35:42 -0300

    frontend added

--- backend/src/services/user.ts
+++ backend/src/services/user.ts
@@ -1,4 +1,4 @@
-import z, { email } from "zod";
+import z from "zod";
 import bcrypt from "bcryptjs";
 import { signinSchema, signupSchema } from "../schemas/auth";
 import { prisma } from "../config/prisma";
